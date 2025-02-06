458e9a1facb1ba94839a3c28f71a6199755e6fd9
Htet Naing Aung <htetnaing.ucsmdy@gmail.com>
2025-02-06 11:56:11 +0630

    Add maven-javadoc-plugin in pom.xml and write comment in UserController.java

--- src/main/java/com/glowmart/shop_management/controller/UserController.java
+++ src/main/java/com/glowmart/shop_management/controller/UserController.java
@@ -14,6 +14,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+/**
+ * Controller for managing users.
+ */
 @RestController
 @RequestMapping(UserAPI.BASE_PATH)
 public class UserController {
@@ -30,7 +33,12 @@ public class UserController {
     @Autowired
     private JwtUtil jwtUtil;
 
-
+    /**
+     * Create or registeration a new user.
+     * @param role Role of user.
+     * @param userDto Data of user.
+     * @return ResponseEntity with a message indicating success or failure.
+     */
     @PostMapping(UserAPI.USER_SIGN_UP)
     public ResponseEntity<?> createUser(@PathVariable("role") String role, @RequestBody UserDto userDto){
         try {
@@ -41,6 +49,12 @@ public class UserController {
         return new ResponseEntity<>("User is successfully created.", HttpStatus.CREATED);
     }
 
+    /**
+     * Login a user.
+     * @param email User's email.
+     * @param password User's password.
+     * @return A JWT token if authentication is successful.
+     */
     @PostMapping(UserAPI.USER_LOGIN)
     public String login(@RequestParam String email, @RequestParam String password) {
 
